13e29e13b36054f0368d2b12307951114ba01c67
ぺんぐいん <ikikaerug10041@yahoo.co.jp>
2022-04-09 17:38:25 +0900

    LBL1セクションの読み込みの終了

--- GalaxyFileLibrary/FileExtentionType/SectionDataSys/LBL1Data/LBL1.cs
+++ GalaxyFileLibrary/FileExtentionType/SectionDataSys/LBL1Data/LBL1.cs
@@ -15,7 +15,8 @@ namespace GalaxyFileLibrary.FileExtentionType.SectionDataSys.LBL1Data
         public int Unknown1 { get; private set; }
         public int Unknown2 { get; private set; }
         public int EntrySize { get; private set; }
-
+        public LabelEntries LabelEntries { get; private set; }
+        public LabelNameEntries LabelNameEntries { get; private set; }
 
         public void Read(BinaryReader br) 
         {
@@ -26,25 +27,88 @@ namespace GalaxyFileLibrary.FileExtentionType.SectionDataSys.LBL1Data
             Unknown2     = BigEndian.ReadInt32(br);
             EntrySize    = BigEndian.ReadInt32(br);
 
-            LabelData[] labelData = new LabelData[EntrySize];
+            LabelEntries = new LabelEntries();
+            LabelEntries.Read(br, EntrySize);
 
-            for (int i = 0; i < EntrySize; i++)
-            {
-                labelData[i].Read(br);
-            }
+            LabelNameEntries = new LabelNameEntries();
+            LabelNameEntries.Read(br,LabelEntries.GetTrueCount());
 
-            Console.WriteLine($"RemoveDuplication: { RemoveDuplication(labelData).Count() }");
+            GalaxyFileStreamLibrary.NintendoBinarySystem.Padding.ReadSkip(br);
 
-            Console.WriteLine(br.BaseStream.Position.ToString("X"));
-        }
-
-        private LabelData[] RemoveDuplication(LabelData[] labelData) 
-        {
-            return labelData.Where(x => x.HashSkip != 0).ToArray();
+            Console.WriteLine("LBL1 End Address: 0x" + br.BaseStream.Position.ToString("X"));
+            Console.WriteLine("----- LBL1 End -----");
         }
     }
 
-    public struct LabelData 
+    public class LabelEntries
+    {
+        public LabelEntry[] FromBinary{ get; private set; }
+        public LabelEntry[] RemoveHashZero { get; private set; }
+
+        public void Read(BinaryReader br , int entrySize) 
+        {
+            FromBinary = new LabelEntry[entrySize];
+
+            for (int entryIndex = 0; entryIndex < entrySize; entryIndex++)
+            {
+                FromBinary[entryIndex].Read(br);
+            }
+
+            RemoveHashZero = RemoveZero(FromBinary);
+        }
+
+        public int GetTrueCount() 
+        {
+            uint labelCount = 0;
+
+            foreach (LabelEntry labelEntry in RemoveHashZero)
+            {
+                labelCount += labelEntry.HashSkip;
+            }
+
+            return (int)labelCount;
+        }
+        
+        private LabelEntry[] RemoveZero(LabelEntry[] labelEntries) 
+        {
+            return labelEntries.Where(labelEntry => labelEntry.HashSkip != 0).ToArray();
+        }
+    }
+
+    public class LabelNameEntries 
+    {
+        public LabelNameEntry[] FromBinary { get; private set; }
+
+        public void Read(BinaryReader br, int trueLabelCount) 
+        {
+            FromBinary = new LabelNameEntry[trueLabelCount];
+
+            for (int j = 0; j < trueLabelCount; j++) 
+            {
+                FromBinary[j].Read(br);
+            }
+        }
+    }
+
+    public struct LabelNameEntry
+    {
+        public int OffsetAddress { get; private set; }
+        public byte TextLength { get; private set; }
+        public string LabelName { get; private set; }
+        public uint LabelID { get; private set; }
+
+        public void Read(BinaryReader br) 
+        {
+            OffsetAddress    = (int)br.BaseStream.Position;
+            TextLength       = br.ReadByte();
+            LabelName        = Encoding.ASCII.GetString(br.ReadBytes(TextLength));
+            LabelID          = BigEndian.ReadUInt32(br);
+
+            //Console.WriteLine(LabelName);
+        }
+    }
+
+    public struct LabelEntry 
     {
         public uint HashSkip { get; private set; }
         public uint Offset { get; private set; }
---
+++ GalaxyFileStreamLibrary/NintendoBinarySystem/Padding.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace GalaxyFileStreamLibrary.NintendoBinarySystem
+{
+    public class Padding
+    {
+        public static void ReadSkip(BinaryReader br) 
+        {
+            while (br.BaseStream.Position % 0x20 != 0) 
+            {
+                br.ReadByte();
+            }
+        }
+    }
+}

453286c2d8cbcd245aa686bc175f60d4f2cdf61f
ぺんぐいん <ikikaerug10041@yahoo.co.jp>
2022-05-29 21:12:28 +0900

    SoundEffectタグを作成するための準備まで

---
+++ GalaxyFileLibrary/FileExtentionType/SectionDataSys/TXT2Data/TagData/SoundSystemTag.cs
@@ -0,0 +1,29 @@
+﻿using GalaxyFileLibrary.FileExtentionType.SectionDataSys.TXT2Data.TagData.SystemTagData;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalaxyFileLibrary.FileExtentionType.SectionDataSys.TXT2Data.TagData
+{
+    public class SoundSystemTag : ITagData
+    {
+        private readonly Dictionary<byte, ISubCategory> _systemTagSubCategory = new Dictionary<byte, ISubCategory>()
+        {
+            //{ 0x00 , new PlayCharacter() }
+        };
+
+        public ISubCategory SubCategory { get; private set; }
+
+        public void Read(BinaryReader br)
+        {
+            byte[] subCategoryBytes = br.ReadBytes(2);
+
+            SubCategory = _systemTagSubCategory[subCategoryBytes[1]];
+
+            SubCategory.Read(br);
+        }
+    }
+}

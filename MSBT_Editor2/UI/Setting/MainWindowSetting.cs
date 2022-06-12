d1e9133f14d650b68aabb45a902ddeebef927894
ぺんぐいん <ikikaerug10041@yahoo.co.jp>
2022-06-12 22:29:38 +0900

    PictureFontクラスの辞書データを外部ファイルから読み込むための準備

--- MSBT_Editor2/UI/Setting/MainWindowSetting.cs
+++ MSBT_Editor2/UI/Setting/MainWindowSetting.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace GalaxyText_Editor.UI.Setting
 {
     public class MainWindowSetting
     {
+        public static Dictionary<string,string> PictureFontListFromTxt{ get; private set; }
         
         public static void Initialize() 
         {
-            
+            PictureFontListFromTxt = new Dictionary<string, string>();
+            GetPictureFontList();
+        }
+
+        private static void GetPictureFontList()
+        {
+            string filePath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+
+
+            File.ReadAllText(filePath);
         }
     }
 }
